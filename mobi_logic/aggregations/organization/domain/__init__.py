f53b58abbcdfac7cc5e1ab9158d5540224533746
Mariusz Masztalerczuk <mariusz@masztalerczuk.com>
2018-06-24 21:13:49 +0200

    Added install() method for bussiness_logi

--- mobi_logic/aggregations/organization/domain/__init__.py
+++ mobi_logic/aggregations/organization/domain/__init__.py
@@ -1,15 +1,7 @@
-
-subscribers = {}
+from mobi_logic import event_queue
 
 
 def publish(event):
-    if type(event) in subscribers:
-        for handler in subscribers[type(event)]:
-            handler(event)
+    event_queue.append(event)
 
 
-def subscribe(event_type, handler):
-    if event_type in subscribers:
-        subscribers[event_type].append(handler)
-    else:
-        subscribers[event_type] = [handler]
