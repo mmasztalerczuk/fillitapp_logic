34ffbeccd8ac7ab86c90a23964168a7bb98f4cb9
Mariusz Masztalerczuk <mariusz@masztalerczuk.com>
2018-07-12 21:03:18 +0200

    Added search research grup in database

--- mobi_logic/aggregations/organization/repositories/unit.py
+++ mobi_logic/aggregations/organization/repositories/unit.py
@@ -1,6 +1,6 @@
 from taranis.abstract.repository import Repository
 
-from mobi_logic.aggregations.organization.domain.entities.unit import Unit
+from mobi_logic.aggregations.organization.domain.entities.unit import Unit, ResearchGroup
 
 
 class UnitRepository(Repository):
@@ -8,6 +8,12 @@ class UnitRepository(Repository):
     def __init__(self, persistent_storage, user_id):
         super(UnitRepository, self).__init__(persistent_storage)
         self._user_id = user_id
+
+    def get_research_group(self, unit_id, research_group_id):
+        properties = (ResearchGroup.user_id == self._user_id,
+                      ResearchGroup.aggregate_id == unit_id,
+                      ResearchGroup.id == research_group_id)
+        return self._persistent_storage.get(ResearchGroup, properties)[0]
 
     def get(self, unit_id):
         properties = (Unit.user_id == self._user_id, Unit.aggregate_id == unit_id)
