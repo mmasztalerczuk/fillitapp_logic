a757fdf639bc597fd3ea25b1c5c485ee713e3867
Mariusz Masztalerczuk <mariusz@masztalerczuk.com>
2018-07-18 17:35:33 +0200

    #1 Implement survey endpoint
     * New version
    
    Signed-off-by: Mariusz Masztalerczuk <mariusz@masztalerczuk.com>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Needed for dependencies
     # install_requires=['numpy'],
     # *strongly* suggested for sharing
-    version='0.1.56',
+    version='0.1.60',
     # The license can be anything you like
     license='MIT',
     description='Bussiness logic for mobi stats',
