9d8c1145ac4632ecff501c86702c4d52e45a08af
Mariusz Masztalerczuk <mariusz@masztalerczuk.com>
2018-08-15 16:37:32 +0200

    2 Implement question endpoint
      * Added question entity
      * Upgraded python version 3.6 -> 3.7
      * Updated pycharm configuration

--- mobi_logic/aggregations/organization/domain/entities/survey.py
+++ mobi_logic/aggregations/organization/domain/entities/survey.py
@@ -1,9 +1,18 @@
+import uuid
 from sqlalchemy.ext.declarative import declarative_base
+from taranis import publish
 from taranis.abstract import DomainEvent
-
+from mobi_logic.aggregations.organization.domain.entities.question import Question
 Base = declarative_base()
 
 
 class Survey():
     class Created(DomainEvent):
         type = "Survey.Created"
+
+    def add_question(self, name):
+        event = Question.Created(id=str(uuid.uuid4()),
+                                 name=name,
+                                 aggregate_id=self.id)
+
+        publish(event)
