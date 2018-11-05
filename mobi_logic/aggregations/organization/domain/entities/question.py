62bda67c71883f6a6f96d902a58d123f9e9b9aab
Mariusz Masztalerczuk <mariusz@masztalerczuk.com>
2018-11-05 18:40:11 +0100

    Respondent

--- mobi_logic/aggregations/organization/domain/entities/question.py
+++ mobi_logic/aggregations/organization/domain/entities/question.py
@@ -1,6 +1,18 @@
+import uuid
+from taranis import publish
 from taranis.abstract import DomainEvent
+
+from mobi_logic.aggregations.organization.domain.entities.response import Response
 
 
 class Question():
     class Created(DomainEvent):
         type = "Question.Created"
+
+    def create_responses(self, value, type, question_id):
+        event = Response.Created(id=str(uuid.uuid4()),
+                               value=value,
+                               type=type,
+                               aggregate_id=question_id)
+
+        publish(event)
--- mobi_logic/aggregations/organization/domain/entities/survey.py
+++ mobi_logic/aggregations/organization/domain/entities/survey.py
@@ -1,18 +1,28 @@
 import uuid
-from sqlalchemy.ext.declarative import declarative_base
 from taranis import publish
 from taranis.abstract import DomainEvent
 from mobi_logic.aggregations.organization.domain.entities.question import Question
-Base = declarative_base()
+from mobi_logic.aggregations.organization.domain.entities.response import Response
 
 
 class Survey():
     class Created(DomainEvent):
         type = "Survey.Created"
 
-    def add_question(self, name):
-        event = Question.Created(id=str(uuid.uuid4()),
+    def add_question(self, name, type, responses):
+        new_question_id = str(uuid.uuid4())
+        event = Question.Created(id=new_question_id,
                                  name=name,
+                                 type=type,
                                  aggregate_id=self.id)
 
         publish(event)
+
+        for response in responses:
+            event = Response.Created(id=str(uuid.uuid4()),
+                             value=response['value'],
+                             type=response['type'],
+                             aggregate_id=new_question_id)
+
+            publish(event)
+
